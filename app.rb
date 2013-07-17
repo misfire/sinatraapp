60f07d0f254027b146a91cc9e36f290a59af2189
danny.chen@tadashishoji.com <dannyc@ITs-MacBook-Pro.local>
2013-07-17 13:30:15 -0700

    initial commit

--- app.rb
+++ app.rb
@@ -2,12 +2,27 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-"Hi from the Sinatra app default route"
+	@name = "Danny"
+	erb :index
 end
 
 get '/vote' do
 "this a vote page"
 end
 
+get '/vote:id' do
 
+end
+
+get '/thanks/' do
+"this is the thank you page"
+end
+
+get '/official-rules' do
+  erb :official_rules
+end
+
+get '/privacy-policy' do
+  erb :privacy_policy
+end
 
