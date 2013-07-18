0ecb2c1b58b3a3c84f5a3b24a3b367c8d07de22d
Ruben Epong <rubenjohne@gmail.com>
2013-07-18 10:44:57 -0700

    Added datamapper and postgresql in heroku

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source :gemcutter
 
 gem "sinatra"
+gem "pg"
+gem "data_mapper"
+gem "dm-postgres-adapter"
+gem "sqlite3"
--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'rubygems'
 require 'sinatra'
+require "data_mapper"
+
+
+# database connection from heroku
+DataMapper.setup(:default, ENV["DATABASE_URL"])
+
+class Group
+  
+  include DataMapper::Resource
+  
+  property  :id,            Serial
+  property  :name,          String, :required => true
+  property  :is_active,     Boolean, :required => true
+  property  :promo_code,    String, :required => true
+  property  :created_at,    DateTime,  :required => false
+  property  :updated_at,    DateTime,  :required => false
+    
+end
+
+# Create or upgrade the database all at once
+DataMapper.auto_upgrade!
+
 
 get '/' do
 "Hi from the Sinatra app default route"
